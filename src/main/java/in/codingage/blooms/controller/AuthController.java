b4d2805bf792941378749f2f83c6c8aa3749675c
Jackey Kumar <jackeyjazzbgp1234@gmail.com>
2026-01-29 02:17:39 +0530

    changes

--- src/main/java/in/codingage/blooms/controller/AuthController.java
+++ src/main/java/in/codingage/blooms/controller/AuthController.java
@@ -7,6 +7,7 @@ import in.codingage.blooms.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+// Aapka React local URL
 @RestController
 @RequestMapping("/api/account")
 public class AuthController {
